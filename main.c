e48468c4053168d34a3226850f6cb8d1fe096f18
wjdwjdwjd1279 <alfzltm9721@naver.com>
2023-11-19 02:23:40 +0900

    11-1

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int i = 10;
+	char c = 'a';
+	
+	int* iptr = &i;
+	char* cptr = &c;
+	int*iptr2 = iptr;
+	
+	printf("i: %p\n%p(size:%i)\n",iptr,&i,sizeof(iptr));
+	printf("c: %p\n%p(size:%i)\n",cptr,&i,sizeof(cptr));
+	printf("iptr2: %p,:%i\n",iptr2,&i,*iptr2);
+	
+	return 0;
+}
