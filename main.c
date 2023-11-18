f67c35bcf9f8684cdda2d868c81ea5da21d619ff
wjdwjdwjd1279 <alfzltm9721@naver.com>
2023-11-19 02:53:12 +0900

    11-4

--- main.c
+++ main.c
@@ -3,19 +3,23 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 	
-void swap(int* x, int* y){
-	int temp = *x;
-	*x = *y;
-	*y = temp;
-}
-	
-int main(int argc, char *argv[]) {
+int main(void) {
 
-	int a = 3;
-	int b =5;
+	int i;
+	int grade[5];
+	int average;
 	
-	swap(&a,&b);
-	printf("a:%i, b:%i\n",a,b);
+	for(i=0;i<5;i++){
+		printf("input value[%i]=",i);
+		scanf("%d",&grade[i]);
+	}
+	
+	for(i=0;i<5;i++){
+		printf("grade[%i]= %i\n",i, *(grade+i));
+		average = average + *(grade+i);
+	}
+	
+	printf("average : %i\n",average);
 	
 	return 0;
 }
